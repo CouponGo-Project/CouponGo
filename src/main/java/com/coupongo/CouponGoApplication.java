52d7b72f64ed8decdcb35092c4867a582582a87f
Bertuğ Kara <bertugkara@outlook.com>
2022-10-04 15:13:29 +0300

    Spring Boot Project Created

---
+++ src/main/java/com/coupongo/CouponGoApplication.java
@@ -0,0 +1,13 @@
+package com.coupongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CouponGoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CouponGoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/coupongo/CouponGoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coupongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CouponGoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
